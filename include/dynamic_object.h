a361bee985857e269c373d1db9ea8a9961c0ae76
TengHooi <chantenghooi@gmail.com>
2022-06-03 16:01:44 +0800

    add DynamicObject class template

--- include/dynamic_object.h
+++ include/dynamic_object.h
@@ -5,6 +5,21 @@
 #include <vector>
 #include <string>
 
+#include <Eigen/Eigen>
 
+#include <ros/ros.h>
+
+class DynamicObject
+{
+private:
+    //Bounding box size vectors for object
+    Eigen::Vector4f box_max_vec_;
+    Eigen::Vector4f box_min_vec_;
+
+
+
+public:
+
+};
 
 #endif //_BOREALIS_MAPPING_DYNAMIC_OBJECT_
