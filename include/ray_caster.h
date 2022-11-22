3f2f7805b4b3db005987301dac70f39b535b9379
TengHooi <chantenghooi@gmail.com>
2022-11-22 18:53:41 +0800

    RayCaster: new members and member mutators

--- include/ray_caster.h
+++ include/ray_caster.h
@@ -10,12 +10,25 @@
 class RayCaster
 {
 private:
-
+    Eigen::Vector3d ray_origin_;
+    Eigen::Vector3d ray_end_;
 
 public:
     RayCaster();
     ~RayCaster();
 
+    void set_ray_origin(Eigen::Vector3d& ray_origin);
+    void set_ray_origin(double& x, double& y, double& z);
+    void set_ray_end(Eigen::Vector3d& ray_end);
+    void set_ray_end(double& x, double& y, double& z);
+
+    void set_end_points(Eigen::Vector3d& ray_origin, Eigen::Vector3d& ray_end);
+
+/*
+    RayCasting2D and RayCasting3D are based on Amanatides & Woo ray tracing algorithm.
+    Their paper "A Fast Voxel Traversal Algorithm for Ray Tracing" can be found here: 
+    <https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.42.3443&rep=rep1&type=pdf>
+*/
     void RayCasting2D();
     void RayCasting3D();
 
