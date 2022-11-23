3780a52237e3e834f0239ae87d3ea455d7dac761
TengHooi <chantenghooi@gmail.com>
2022-11-23 14:35:15 +0800

    RayCaster: add mutators declaration

--- src/ray_caster.cpp
+++ src/ray_caster.cpp
@@ -7,6 +7,31 @@ RayCaster::RayCaster()
 
 RayCaster::~RayCaster() {}
 
+void RayCaster::set_ray_origin(Eigen::Vector3d& ray_origin)
+{
+    
+}
+
+void RayCaster::set_ray_origin(double& x, double& y, double& z)
+{
+
+}
+
+void RayCaster::set_ray_end(Eigen::Vector3d& ray_end)
+{
+
+}
+
+void RayCaster::set_ray_end(double& x, double& y, double& z)
+{
+
+}
+
+void RayCaster::set_end_points(Eigen::Vector3d& ray_origin, Eigen::Vector3d& ray_end)
+{
+
+}
+
 void RayCaster::RayCasting2D()
 {
 
