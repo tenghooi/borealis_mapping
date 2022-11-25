42cd7827e27eb14a4dd05fd35c65edbec30e088a
TengHooi <chantenghooi@gmail.com>
2022-11-25 15:52:03 +0800

    RayCaster: add comment to RayCasting()

--- include/ray_caster.h
+++ include/ray_caster.h
@@ -28,6 +28,13 @@ public:
     RayCasting2D and RayCasting3D are based on Amanatides & Woo ray tracing algorithm.
     Their paper "A Fast Voxel Traversal Algorithm for Ray Tracing" can be found here: 
     <https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.42.3443&rep=rep1&type=pdf>
+
+    This algorithm uses a parameterized line equation for ray tracing.
+    Ray equation:  r = r0 + t*v, where r0 is the ray origin, v is difference between ray end and ray origin.
+    
+    E.g: X0 = [0.5 0.5], X1 = [2.5 0]
+         Then, v = [2 -0.5] 
+         r = [0.5 0.5] + t * [2 -0.5]
 */
     void RayCasting2D();
     void RayCasting3D();
